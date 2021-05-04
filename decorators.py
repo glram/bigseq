1dcf2e9be757d4cb3934410d213bae1c65669210
Kaveri <kaveri.nadhamuni@gmail.com>
2021-05-04 16:01:27 -0400

    more debugging graph

--- decorators.py
+++ decorators.py
@@ -1,15 +1,18 @@
-
-
+from __future__ import print_function
+from functools import wraps
 from bigsi.utils import convert_query_kmers
 from bigsi.utils import convert_query_kmer
+import logging
 import time
 import collections
 import itertools
 
+logging.basicConfig(level=logging.DEBUG)
 
+logger = logging.getLogger(__name__)
+from bigsi.utils import DEFAULT_LOGGING_LEVEL
 
-
-
+logger.setLevel(DEFAULT_LOGGING_LEVEL)
 
 
 def choose_convert_func(kmers):
@@ -28,10 +31,8 @@ def kmers_or_bytes(self, kmers):
     else:
         return kmers
 
-@python
+
 def convert_kmers(func):
-    from functools import wraps
-    # todo: wut is wraps???
     # Wrapper for functions of the form (self, kmers, colour, min_lexo=False)
     # or (self, kmers, min_lexo=False) and returns only the min of the kmer
     # and it's reverse complement
@@ -47,9 +48,8 @@ def convert_kmers(func):
 
     return convert_kmers_inner
 
-@python
+
 def convert_kmers_to_canonical(func):
-    from functools import wraps
     # Wrapper for functions of the form (self, kmers, colour, min_lexo=False)
     # or (self, kmers, min_lexo=False) and returns only the min of the kmer
     # and it's reverse complement
